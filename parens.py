ee4f6b228ef8e45e5be596a5a2b750ad8cf6f9f1
Anderson Mills <nodog@konfuzo.net>
2023-07-05 21:33:31 -0500

    Update for dangling open paren

--- parens.py
+++ parens.py
@@ -1,6 +1,6 @@
-def remove_invalid_parens(s: str) -> str:
+def remove_invalid_parens_iter(s: str) -> str:
     # for now it will always be open-close, everything else goes,
-    # and there will be no dangling open parens
+    # and there can be dangling open parens which should be removed
     removed_s = ''
     parens_closed = True
     for c in s:
@@ -8,7 +8,6 @@ def remove_invalid_parens(s: str) -> str:
         if c == '(':
             if parens_closed == True:
                 parens_closed = False
-
             else:
                 add_c = False
         elif c == ')':
@@ -19,18 +18,29 @@ def remove_invalid_parens(s: str) -> str:
         if add_c:
             removed_s += c
 
+    # remove last unclosed open paren
+    last_open = removed_s.rfind('(')
+    last_close = removed_s.rfind(')')
+
+    if last_open != -1:
+        if last_close < last_open:
+            # remove last open
+            removed_s = removed_s[:last_open] + removed_s[last_open+1:]
+
     return removed_s
 
 
 def test_paren_remove(s: str) -> str:
     print("----")
     print(f"input: {s}")
-    print(f"output: {remove_invalid_parens(s)}")
+    print(f"output: {remove_invalid_parens_iter(s)}")
 
 
 if __name__ == '__main__':
     print("---parens---")
     test_paren_remove("()()")
     test_paren_remove(")()")
+    test_paren_remove(")()(")
     test_paren_remove("(())")
     test_paren_remove("(((((((() (((((()")
+    test_paren_remove("()))))) ())))) (((((")
